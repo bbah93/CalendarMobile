aa18972eee436b2fb501637d08529377e9d7dd61
Bouabacar <boubacarbah@ac.c4q.nyc>
2018-06-20 20:44:04 -0400

    added day fragment

---
+++ app/src/main/java/nyc/c4q/calendarmobile/MonthFragment.java
@@ -0,0 +1,32 @@
+package nyc.c4q.calendarmobile;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MonthFragment extends Fragment {
+
+
+    public MonthFragment() {
+        // Required empty public constructor
+    }
+
+    View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_month, container, false);
+
+        return rootView;
+    }
+
+}
